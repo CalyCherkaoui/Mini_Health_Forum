e4a2cd4f94c3d47430ef4bd402a57cce74c811fc
houda cherkaoui <houda.cherkaoui@gmail.com>
2020-07-28 14:03:24 -0500

    Codinf form for  question_form

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,8 @@ class HomeController < ApplicationController
 
   def about
   end
+
+  def temp
+    redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
 
   root 'home#index'
+
   get 'home/index'
+
   get '/about' => 'home#about'
+
+  post '/questions' => 'home#temp'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
