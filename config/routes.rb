1207f11a1c7892287f71a67794c61b57f11a4247
houda cherkaoui <houda.cherkaoui@gmail.com>
2020-07-28 14:39:16 -0500

    Adding modal for answers view

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
 
   post '/questions' => 'home#temp'
 
+  get '/questions/:id' => 'home#question'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
