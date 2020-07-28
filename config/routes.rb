c2e4d7bb9f0e749e76b1e4c9f0386ba280b65934
houda cherkaoui <houda.cherkaoui@gmail.com>
2020-07-28 12:17:08 -0500

    Adding Bootstrap functionalities

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
 
   root 'home#index'
   get 'home/index'
+  get '/about' => 'home#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
